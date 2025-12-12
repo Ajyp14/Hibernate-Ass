392fb1a80274a3c6fff8cfb357edef0317db49ae
Ajyp14 <ajyp1404@gmail.com>
2025-12-12 19:16:05 +0530

    Add files via upload

---
+++ Ass05-First-hib-App/src/main/java/com/tca/Ass05_First_hib_App/App.java
@@ -0,0 +1,13 @@
+package com.tca.Ass05_First_hib_App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
