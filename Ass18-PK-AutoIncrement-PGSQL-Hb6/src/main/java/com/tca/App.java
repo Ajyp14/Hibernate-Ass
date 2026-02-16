f0aac53fcbc08cbf854520c6d849065acddd5f37
Ajay pawar <ajyp1404@gmail.com>
2026-02-16 23:36:15 +0530

    Add files via upload

---
+++ Ass18-PK-AutoIncrement-PGSQL-Hb6/src/main/java/com/tca/App.java
@@ -0,0 +1,51 @@
+
+package com.tca;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.tca.entities.Student;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration=null;
+        SessionFactory factory=null;
+        Session session=null;
+        Transaction tr=null;
+        
+         try {
+        	 configuration=new Configuration();
+        	 configuration.configure();
+        	 configuration.addAnnotatedClass(Student.class);
+        	 
+        	 factory=configuration.buildSessionFactory();
+        	 session=factory.openSession();
+        	 tr=session.beginTransaction();
+        	 
+        	 Student st=new Student();
+        	// st.setRno(101);
+        	 st.setName("ppppp");
+        	 st.setPer(97.45);
+        	 
+        	 int trno = (int) session.save(st);
+        	 
+        	 System.out.println("Record saved for rool number : "+trno);
+        	 
+        	 tr.commit();
+        	 
+         }catch (Exception e) {
+        	 tr.rollback();
+			e.printStackTrace();
+		}finally {
+			session.close();
+			factory.close();
+		}
+        
+        
+        
+    }
+}
