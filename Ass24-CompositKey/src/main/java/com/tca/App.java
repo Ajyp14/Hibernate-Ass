9fdd40f8bd123bee7f16039e4cff0863d553e12e
Ajay pawar <ajyp1404@gmail.com>
2026-04-09 23:28:16 +0530

    Add files via upload

---
+++ Ass24-CompositKey/src/main/java/com/tca/App.java
@@ -0,0 +1,68 @@
+package com.tca;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.tca.entities.Student;
+import com.tca.entities.StudentCompositeKey;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = null;
+        SessionFactory factory=null;
+        Session session=null;
+        Transaction tr=null;
+        
+        try {
+        	configuration=new Configuration();
+        	configuration.configure();
+        	
+        	factory=configuration.buildSessionFactory();
+        	session=factory.openSession();
+        	tr=session.beginTransaction();
+        	
+//   How to Insert A record
+        	
+//        	StudentCompositeKey key=new StudentCompositeKey();
+//        	key.setFname("sachin");
+//        	key.setLname("Tendulkar");
+//        	
+//        	Student st=new Student();
+//        	st.setId(key);
+//        	st.setCity("Pune");
+//        	
+//        	session.persist(st);
+//        	
+//        	System.out.println(session.getIdentifier(st));
+//---------------------------------------------------------------------------------------------------------
+        	
+//      How to fetch a specific record
+        	
+        	StudentCompositeKey key=new StudentCompositeKey();
+        	key.setFname("Sachin");
+        	key.setLname("Tendulkar");
+        	
+        	Student st = session.load(Student.class,key);
+        	
+        	System.out.println("City name : "+st.getCity());
+        	System.out.println(" Rec : "+st.getId().getFname());
+        	
+        	tr.commit();
+        	
+        }catch (Exception e) {
+        	tr.rollback();
+			e.printStackTrace();
+		}finally {
+			factory.close();
+			session.close();
+		}
+        
+        
+        
+        
+    }
+}
